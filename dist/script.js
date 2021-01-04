3f8b8c0bdf5513cbe73d0d4972b3608f4facafad
Alexey Tarasov <tarasov@skillfolio.ru>
2021-01-04 14:36:39 +0300

    dsp755: add new sounds

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,36 @@
+const chords = {
+  C: {
+    keys: [ 'C1', 'E1', 'G1' ],
+  },
+  F: {
+    keys: [ 'C1', 'F1', 'A1' ],
+  },
+  G: {
+    keys: [ 'D1', 'G1', 'B1' ],
+  },
+  Am: {
+    keys: [ 'E1', 'A1', 'C2' ],
+  },
+  Bb: {
+    keys: [ 'D1', 'F1', 'Bb1' ],
+  },
+}
+
+const notes = {
+  C1: new Audio('sounds/C1.wav'),
+  Db1: new Audio('sounds/Db1.wav'),
+  D1: new Audio('sounds/D1.wav'),
+  Eb1: new Audio('sounds/Eb1.wav'),
+  E1: new Audio('sounds/E1.wav'),
+  F1: new Audio('sounds/F1.wav'),
+  Gb1: new Audio('sounds/Gb1.wav'),
+  G1: new Audio('sounds/G1.wav'),
+  Ab1: new Audio('sounds/Ab1.wav'),
+  A1: new Audio('sounds/A1.wav'),
+  Bb1: new Audio('sounds/Bb1.wav'),
+  B1: new Audio('sounds/B1.wav'),
+  C2: new Audio('sounds/C2.wav'),
+}
 
   let currentChord = 1
   const keys = document.querySelector('.keys')
@@ -10,6 +43,8 @@
   const tonalityButtons = Array.from(document.querySelectorAll('.tonality-button'))
   const fullScreen = document.querySelector('#fullScreen');
   const activeKeys = [ 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN' ]
+  let noteLetter = 'C1'
+  let audio = notes[noteLetter]
 
   const alreadyPressed = [];
 
@@ -34,27 +69,38 @@
   function onKeyClick(e) {
     const key = e.target.closest('button');
     key.classList.add("playing");
-    const audio = document.querySelector(`audio[data-code="${key.dataset.code}"]`);
+    if (key.dataset.chord) {
+      const chordLetter = document.querySelector(`button[data-code="${key.dataset.code}"]`).dataset.chord;
+      const chordKeys = chords[chordLetter].keys
+      chordKeys.map(key => {
+        notes[key].currentTime = 0
+        notes[key].play()
+      })
+      setTimeout(() => { key.classList.remove("playing") }, 100)
+      if (key.id === 'chord1') {
+        currentChord = 1
+        glowChordOnClick()
+      } else if (key.id === 'chord2') {
+        currentChord = 2
+        glowChordOnClick()
+      } else if (key.id === 'chord3') {
+        currentChord = 3
+        glowChordOnClick()
+      }  else if (key.id === 'chord4') {
+        currentChord = 4
+        glowChordOnClick()
+      } else if (key.id === 'chord5') {
+        currentChord = 5
+        glowChordOnClick()
+      }
+      return
+    }
+    const noteLetter = document.querySelector(`button[data-code="${key.dataset.code}"]`).dataset.note;
+    const audio = notes[noteLetter]
     audio.currentTime = 0;
     audio.play();
     setTimeout(() => { key.classList.remove("playing") }, 100)
 
-    if (key.id === 'chord1') {
-    currentChord = 1
-    glowChordOnClick()
-  } else if (key.id === 'chord2') {
-    currentChord = 2
-    glowChordOnClick()
-  } else if (key.id === 'chord3') {
-    currentChord = 3
-    glowChordOnClick()
-  }  else if (key.id === 'chord4') {
-    currentChord = 4
-    glowChordOnClick()
-  } else if (key.id === 'chord5') {
-    currentChord = 5
-    glowChordOnClick()
-  }
   }
 
   function removeTransition(e) {
@@ -63,51 +109,19 @@
     key.classList.remove('playing', 'glowKey', 'glowChord', 'chordIndicatorOn');
   }
 
-  let audio = document.querySelector(`audio[data-code="KeyA"]`);
-
-  function playSound(e) {
-
-    if (e.code === 'KeyX') {
-      currentChord = 1
-      let letter = document.querySelector(`#chordLetter${currentChord}`).textContent.trim()
-      chords[letter].sound.currentTime = 0
-      chords[letter].sound.play()
-      glowChordOnKey()
-    } else if (e.code === 'KeyC') {
-      currentChord = 2
-      let letter = document.querySelector(`#chordLetter${currentChord}`).textContent.trim()
-      chords[letter].sound.currentTime = 0
-      chords[letter].sound.play()
-      glowChordOnKey()
-    } else if (e.code === 'KeyV') {
-      currentChord = 3
-      let letter = document.querySelector(`#chordLetter${currentChord}`).textContent.trim()
-      chords[letter].sound.currentTime = 0
-      chords[letter].sound.play()
-      glowChordOnKey()
-    }  else if (e.code === 'KeyB') {
-      currentChord = 4
-      let letter = document.querySelector(`#chordLetter${currentChord}`).textContent.trim()
-      chords[letter].sound.currentTime = 0
-      chords[letter].sound.play()
-      glowChordOnKey()
-    } else if (e.code === 'KeyN') {
-      currentChord = 5
-      let letter = document.querySelector(`#chordLetter${currentChord}`).textContent.trim()
-      chords[letter].sound.currentTime = 0
-      chords[letter].sound.play()
-      glowChordOnKey()
-    } else {
-      audio = document.querySelector(`audio[data-code="${e.code}"]`);
-      audio.currentTime = 0;
-      audio.play();
-    }
-    const key = document.querySelector(`button[data-code="${e.code}"]`);
-
-    key.classList.add('playing');
-}
-
 function onKeyDown(e) {
+
+  let element = document.querySelector(`button[data-code="${e.code}"]`)
+
+  if (document.querySelector(`button[data-code="${e.code}"]`).dataset.note) {
+
+    let note = document.querySelector(`button[data-code="${e.code}"]`).dataset.note
+
+    element.classList.add('playing')
+
+    notes[note].currentTime = 0
+    notes[note].play()
+  }
 
   if (!activeKeys.includes(e.code)) {
     return
@@ -115,6 +129,10 @@ function onKeyDown(e) {
 
   if (alreadyPressed.includes(e.code)) return;
   alreadyPressed.push(e.code);
+
+  element.classList.add('playing')
+
+  let chord = document.querySelector(`button[data-code="${e.code}"]`).dataset.chord
 
   if (e.code === 'KeyX') {
     currentChord = 1
@@ -132,8 +150,6 @@ function onKeyDown(e) {
     currentChord = 5
     glowChordOnKey()
   }
-
-  playSound(e)
 }
 
 function onKeyUp(e) {
@@ -145,7 +161,6 @@ function onKeyUp(e) {
   let pressedKey = alreadyPressed.indexOf(e.code)
   alreadyPressed.splice(pressedKey, 1)
   let key = document.querySelector(`[data-code=${e.code}]`)
-  let chord = 
   key.classList.remove('playing', 'glowKey', 'glowChord', 'chordIndicatorOn');
 
   if (e.code === 'KeyX') {
@@ -168,7 +183,7 @@ function onKeyUp(e) {
 
   background.addEventListener('transitionend', removeTransition);
   background.addEventListener('click', function() {
-    body.classList.toggle('body2');
+    body.classList.toggle('background2');
   });
 
   function glowChordOnKey() {
@@ -183,8 +198,8 @@ function onKeyUp(e) {
 }
 
 function glowChordOnClick() {
-  let letter = document.querySelector(`#chordLetter${currentChord}`).textContent.trim()
   let chordElement = document.querySelector(`#chordLetter${currentChord}`)
+  let letter = chordElement.textContent.trim()
   
   if (chords[letter]) {
     chords[letter].keys.map(key => document.querySelector(`div[data-note="${key}"`).classList.add('glowChord'))
@@ -206,30 +221,6 @@ function glowChordOff() {
       chordElement.classList.remove('chordIndicatorOn');
     }, 500)
 
-}
-
-
-const chords = {
-  C: {
-    keys: [ 'C1', 'C2', 'G2', 'C3', 'E3', 'G3' ],
-    sound: new Audio('sounds/chordC.wav')
-  },
-  F: {
-    keys: [ 'F1', 'F2', 'A2', 'C3', 'F3', 'A3' ],
-    sound: new Audio('sounds/chordF.wav')
-  },
-  G: {
-    keys: [ 'G1', 'G2', 'B2', 'D3', 'G3' ],
-    sound: new Audio('sounds/chordG.wav')
-  },
-  Am: {
-    keys: [ 'A1', 'E2', 'A2', 'C3', 'E3' ],
-    sound: new Audio('sounds/chordAm.wav')
-  },
-  Bb: {
-    keys: [ 'Bb1', 'F2', 'Bb2', 'D3', 'F3' ],
-    sound: new Audio('sounds/chordBb.wav')
-  },
 }
 
 
