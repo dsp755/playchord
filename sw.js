ade414511a91a00ff3fe44aa24aba13b776575dd
dsp755 <people13@ya.ru>
2021-02-28 17:02:11 +0300

    dsp755: modify sw.js

--- sw.js
+++ sw.js
@@ -29,3 +29,31 @@ async function cacheFirst(req) {
   const cachedResponse = await cache.match(req); 
   return cachedResponse || fetch(req); 
 }
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  if (/.*(json)$/.test(req.url)) {
+    event.respondWith(networkFirst(req));
+  } else {
+    event.respondWith(cacheFirst(req));
+  }
+});
+
+async function networkFirst(req) {
+  const cache = await caches.open(cacheName);
+  try { 
+    const fresh = await fetch(req);
+    cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) { 
+    const cachedResponse = await cache.match(req);
+    return cachedResponse;
+  }
+}
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || networkFirst(req);
+}
